f438f9ddb877d050faad4bde2af9851177b4a753
iOSayedAhmed <elsayed1ahmed0@gmail.com>
2022-01-14 17:13:01 +0200

    Initial Commit

---
+++ Mahamy|مهامي/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mahamy|مهامي
+//
+//  Created by Develop on 1/14/22.
+//  Copyright © 2022 Develop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
